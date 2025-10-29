511f3423eadf438c114b740f140a5928fa890ad4
Tron2704 <rohitmodak400@gmail.com>
2025-10-29 19:23:39 +0530

    Revert Changes

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,3 +1,3 @@
 export default function Result() {
-  return <h1 className="text-center text-blac text-2xl mt-10">Result Page</h1>;
+  return <h1 className="text-center text-black text-2xl mt-10">Result Page</h1>;
 }
